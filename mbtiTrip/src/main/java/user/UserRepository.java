a2211cddefd813eb6bfa0e746f73947063adb71d
hyunseok942 <9417743@naver.com>
2024-03-08 13:51:31 +0900

    r-userRepository 0308 김현석

---
+++ mbtiTrip/src/main/java/user/UserRepository.java
@@ -0,0 +1,10 @@
+package user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Integer> {
+
+	Optional<SiteUser> findByUsername(String username);
+}
