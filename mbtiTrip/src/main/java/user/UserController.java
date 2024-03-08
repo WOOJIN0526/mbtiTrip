b4639b25dc9b37d1ae81e5f5cc9eabf55ecdbac2
hyunseok942 <9417743@naver.com>
2024-03-08 12:47:35 +0900

    r-userController 클래스 0308 김현석

---
+++ mbtiTrip/src/main/java/user/UserController.java
@@ -0,0 +1,44 @@
+package user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import jakarta.validation.Valid;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/signup") 
+	public String signup(UserCreateForm userCreateForm) {
+		return "signup";
+	}
+	
+	@PostMapping("/signup") 
+	public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "signup";
+		}
+		
+		if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+			return "signup";
+		}
+		
+		
+		this.userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1(), userCreateForm.getMbti());
+		return "redirect:/";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login_form";
+	}
+}
