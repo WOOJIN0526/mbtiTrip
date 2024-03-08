c2bbebd055d56e9fde928bfe3d16c1ee84ac1bb1
Shin-sungjin <tlstjdwls0906@gmail.com>
2024-03-08 22:26:13 +0900

    U-mbti entity 0308신성진

--- mbtiTrip/src/main/java/mbti/mbti.java
+++ mbtiTrip/src/main/java/mbti/mbti.java
@@ -1,5 +1,7 @@
 package mbti;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+/*3월 8일 작업자 신성진 */
 
 @Getter
 @Setter
@@ -15,10 +18,10 @@ public class mbti {
 	// mbti
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Integer mbti_id;
 	
 	@Column(name = "mbti_list")
-	private String mbti;
+	private mbti mbti;
 }
 /* 3월 8일 작업자 신성진
  * mbti ENtitiy 생성 
