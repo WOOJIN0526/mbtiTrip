65c14bdb9880d825f5859b285e72c24b904fe494
Shin-sungjin <tlstjdwls0906@gmail.com>
2024-03-08 16:00:13 +0900

    신성진 주석

--- mbtiTrip/src/main/java/mbti/mbtiService.java
+++ mbtiTrip/src/main/java/mbti/mbtiService.java
@@ -16,7 +16,10 @@ public class mbtiService {
 	private UserRepository userRepo;
 	
 	
-	/*3월 8일 작업자 신성진, 사이트 이용자의 mbti 설정 메소드 설정 */
+	/*3월 8일 작업자 신성진, 사이트 이용자의 mbti 설정 메소드 설정 
+	 * mbti 데이터는 미리 설정해두고, 땡겨와서 User에 넣는 방식으로 수정할 예정
+	 */
+	
 	public void setUserMbti(SiteUser User, String mbti ) {
 		SiteUser user = new SiteUser();
 		user.setMbti(mbti);
