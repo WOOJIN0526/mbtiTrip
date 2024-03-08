4664a445e2453e51174c9e6ff6d8dd0c7c3f5923
hyunseok942 <9417743@naver.com>
2024-03-08 15:56:11 +0900

    c-post_question 엔티티 0308 김현석

---
+++ mbtiTrip/src/main/java/post/post_question.java
@@ -0,0 +1,44 @@
+package post;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class post_question {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer user_id;
+	
+	@Column(length = 100)
+	private String subject; // 제목
+
+	@Column(columnDefinition = "TEXT")
+	private String content; // 내용
+	
+	@Column(length=4)
+	private String mbti; //mbti
+	
+	private LocalDateTime createDate; // 작성일자
+	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	private List<post_answer> answerList;
+	
+	@ManyToOne
+	private user.SiteUser author;
+	
+	private LocalDateTime modifyDate;
+}
