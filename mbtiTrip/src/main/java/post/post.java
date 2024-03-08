c0e895cd39fa94c0a7a3a320948e1457b27f1b30
hyunseok942 <9417743@naver.com>
2024-03-08 14:28:34 +0900

    c- post 엔티티 0308 김현석

---
+++ mbtiTrip/src/main/java/post/post.java
@@ -0,0 +1,49 @@
+package post;
+
+import java.time.LocalDateTime;
+
+import com.example.test.SiteUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer user_id; 
+	
+	@Column(unique = true) //중복허용 x
+	private String username;
+	
+	@Column(unique = true)
+	private Integer post_category_id;
+	
+	@ManyToOne
+	private user.SiteUser author;
+	
+	private String title;
+	
+	private String views;
+	
+	private String contents;
+	
+	private String answerList;
+	
+	private String suggestion;
+	
+	private String unsuggestion;
+	
+	private LocalDateTime createDate;
+	
+	private LocalDateTime modifyDate;
+}
