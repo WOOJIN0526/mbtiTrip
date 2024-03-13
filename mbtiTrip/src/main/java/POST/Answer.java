9a101ffc22e009eac66388dc284dc25f46a355bb
hyunseok942 <9417743@naver.com>
2024-03-13 22:33:17 +0900

    c-answer 엔티티 0313 김현석

---
+++ mbtiTrip/src/main/java/POST/Answer.java
@@ -0,0 +1,42 @@
+package POST;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+	@Id @GeneratedValue
+	@Column(name="Answer_Key")
+	private Integer answer_id;
+	
+	@Column(name="Post_ID")
+	private Integer Post_id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String content;
+	
+	@ManyToOne
+	private Post post; //Answer <-> Post
+	
+	//@ManyToOne
+	//private User author; user 엔티티부분 마무리시 수정
+	
+	//@ManyToMany
+	//Set<User> voter; user 엔티티부분 마무리시 수정
+	
+	
+	private LocalDateTime answer_updateDate;
+	
+	
+	private LocalDateTime answer_modifiDate;
+}
