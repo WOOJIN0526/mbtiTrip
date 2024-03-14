4989576bd87a8da39ed0b9aab48ac8a380e130fd
Shin-sungjin <tlstjdwls0906@gmail.com>
2024-03-14 19:34:22 +0900

    03-14 myBatis 연결 테스트

---
+++ mbtiTrip/src/main/java/com/example/test/User/UserDAO.java
@@ -0,0 +1,22 @@
+package com.example.test.User;
+
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO {
+
+	@Autowired
+	SqlSessionTemplate sqlSessiontemplate;
+	
+	public int insert(Map<String, Object> user) {
+		int result = sqlSessiontemplate.insert("user.insert", user);
+		return result;
+	};
+	
+	
+	
+}
