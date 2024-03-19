c303d097f42bcd247694e025c39a0e3760b6b5a7
Shin-sungjin <tlstjdwls0906@gmail.com>
2024-03-19 13:59:24 +0900

    U -

--- mbtiTrip/src/main/java/com/example/test/User/Controller/UserController.java
+++ mbtiTrip/src/main/java/com/example/test/User/Controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.test.User.Controller;
 
+import java.io.Console;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.test.User.DTO.UserDTO;
@@ -17,17 +20,17 @@ public class UserController {
 	UserService userSerivice; 
 	
 	@RequestMapping(value = "/signup", method=RequestMethod.GET)
-	public ModelAndView singup() {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("sign_up");
-		return mav;
+	public String signUp() {
+		return "sign_up";
 	}
 	
+	
 	@RequestMapping(value = "/signup", method=RequestMethod.POST)
+	@ResponseBody
 	public ModelAndView singup(@ModelAttribute UserDTO userdto) {		
 		ModelAndView mav = new ModelAndView();
-		mav.addObject(userdto);
-		mav.setViewName("main");
+		System.out.print(userdto);
+		mav.setViewName("login_form");
 		return mav;
 	}
 }
