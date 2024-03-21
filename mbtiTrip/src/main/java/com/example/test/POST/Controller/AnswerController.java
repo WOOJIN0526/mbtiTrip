3865ba66fa75103dcef16daadd86cff3a3801a55
hyunseok942 <9417743@naver.com>
2024-03-21 17:46:54 +0900

    c,u,d- 0321 김현석

--- mbtiTrip/src/main/java/com/example/test/POST/Controller/AnswerController.java
+++ mbtiTrip/src/main/java/com/example/test/POST/Controller/AnswerController.java
@@ -29,81 +29,81 @@ import groovyjarjarpicocli.CommandLine.Model;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-//@RequestMapping("/answer")
-//@RequiredArgsConstructor
-//@Controller
-//public class AnswerController {
-//
-//		@Autowired
-//		AnswerService answerService;
-//		
-//		@Autowired
-//		PostService postservice;
-//		
-//		@Autowired
-//		UserService userservice;
-//	
-//		//@PreAuthorize("isAuthenticated()")
-//	    @PostMapping("/create/{id}")
-//	    public String createAnswer(Model model, @PathVariable("id") Integer id, 
-//	            @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
-//	        PostDTO postDto = this.postservice.getPost(id);
-//	        UserDTO UserDto = this.userservice.getUser(principal.getName());
-//	        if (bindingResult.hasErrors()) {
-//	            model.addAttribute("post", postDto);
-//	            return "post_detail";
-//	        }
-//	        AnswerDTO answerDto = this.answerService.create(postDto, 
-//	                answerForm.getContent(), UserDto);
-//	        return String.format("redirect:/post/detail/%s#answer_%s", 
-//	                answerDto.getPost().getPostID(), answerDto.getPostID());
-//	    }
-//	    
-//	    //@PreAuthorize("isAuthenticated()")
-//	    @GetMapping("/modify/{id}")
-//	    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
-//	        AnswerDTO answerDto = this.answerService.getAnswer(id);
-//	        if (!answerDto.getAuthor().getUserName().equals(principal.getName())) {
-//	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-//	        }
-//	        answerForm.setContent(answerDto.getContent());
-//	        return "answer_form";
-//	    }
-//	    
-//	    //@PreAuthorize("isAuthenticated()")
-//	    @PostMapping("/modify/{id}")
-//	    public String answerModify(@Valid AnswerForm answerForm, @PathVariable("id") Integer id,
-//	            BindingResult bindingResult, Principal principal) {
-//	        if (bindingResult.hasErrors()) {
-//	            return "answer_form";
-//	        }
-//	        AnswerDTO answerDto = this.answerService.getAnswer(id);
-//	        if (!answerDto.getAuthor().getUserName().equals(principal.getName())) {
-//	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-//	        }
-//	        this.answerService.modify(answerDto, answerForm.getContent());
-//	        return String.format("redirect:/post/detail/%s#answer_%s", 
-//	                answerDto.getPost().getPostID(), answerDto.getPostID());
-//	    }
-//	    
-//	    //@PreAuthorize("isAuthenticated()")
-//	    @GetMapping("/delete/{id}")
-//	    public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
-//	        AnswerDTO answerDto = this.answerService.getAnswer(id);
-//	        if (!answerDto.getAuthor().getUserName().equals(principal.getName())) {
-//	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
-//	        }
-//	        this.answerService.delete(answerDto);
-//	        return String.format("redirect:/post/detail/%s", answerDto.getPost().getPostID());
-//	    }
-//	    
-//	    //@PreAuthorize("isAuthenticated()")
-//	    @GetMapping("/vote/{id}")
-//	    public String answerVote(Principal principal, @PathVariable("id") Integer id) {
-//	        AnswerDTO answerDto = this.answerService.getAnswer(id);
-//	        UserDTO siteUserDto = this.userservice.getUser(principal.getName());
-//	        this.answerService.vote(answerDto, siteUserDto);
-//	        return String.format("redirect:/post/detail/%s#answer_%s", 
-//	                answerDto.getPost().getPostID(), answerDto.getPostID());
-//	    }
-//}
+@RequestMapping("/answer")
+@Controller
+public class AnswerController {
+
+		@Autowired
+		AnswerService answerService;
+		
+		@Autowired
+		PostService postservice;
+		
+		@Autowired
+		UserService userservice;
+	
+  //@PreAuthorize("isAuthenticated()")
+	@PostMapping("/create/{id}")
+	public String createAnswer(Model model, @PathVariable("id") Integer id, 
+	@Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
+	PostDTO postDto = this.postservice.getPost(id);
+	UserDTO userDto = this.userservice.findByUserName(principal.getName());
+		if (bindingResult.hasErrors()) {
+		            model.addAttribute("post", postDto);
+		            return "question_detail";
+		        }
+		        AnswerDTO answerDto = this.answerService.create(postDto, 
+		                answerForm.getContent(), userDto);
+		        return String.format("redirect:/question/detail/%s#answer_%s", 
+		                answerDto.getPost().getUserId(), answerDto.getAnswerID());
+		    }
+		    
+		    //@PreAuthorize("isAuthenticated()")
+		    @GetMapping("/modify/{id}")
+		    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
+		        AnswerDTO answerDto = this.answerService.getAnswer(id);
+		        if (!answerDto.getAuthor().getUserName().equals(principal.getName())) {
+		            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+		        }
+		        answerForm.setContent(answerDto.getContent());
+		        return "answer_form";
+		    }
+		    
+		    //@PreAuthorize("isAuthenticated()")
+		    @PostMapping("/modify/{id}")
+		    public String answerModify(@Valid AnswerForm answerForm, @PathVariable("id") Integer id,
+		            BindingResult bindingResult, Principal principal) {
+		        if (bindingResult.hasErrors()) {
+		            return "answer_form";
+		        }
+		        AnswerDTO answerDto = this.answerService.getAnswer(id);
+		        if (!answerDto.getAuthor().getUserName().equals(principal.getName())) {
+		            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+		        }
+		        this.answerService.modify(answerDto, answerForm.getContent());
+		        return String.format("redirect:/question/detail/%s#answer_%s", 
+		                answerDto.getPost().getUserId(), answerDto.getAnswerID());
+		    }
+		    
+		    //@PreAuthorize("isAuthenticated()")
+		    @GetMapping("/delete/{id}")
+		    public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
+		        AnswerDTO answerDto = this.answerService.getAnswer(id);
+		        if (!answerDto.getAuthor().getUserName().equals(principal.getName())) {
+		            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+		        }
+		        this.answerService.delete(answerDto);
+		        return String.format("redirect:/question/detail/%s", answerDto.getPost().getUserId());
+		    }
+		    
+		    //@PreAuthorize("isAuthenticated()")
+		    @GetMapping("/vote/{id}")
+		    public String answerVote(Principal principal, @PathVariable("id") Integer id) {
+		        AnswerDTO answerDto = this.answerService.getAnswer(id);
+		        UserDTO siteUserDto = this.userservice.findByUserName(principal.getName());
+		        this.answerService.vote(answerDto, siteUserDto);
+		        return String.format("redirect:/question/detail/%s#answer_%s", 
+		                answerDto.getPost().getUserId(), answerDto.getAnswerID());
+		    }
+
+}
--- mbtiTrip/src/main/java/com/example/test/POST/Service/PostService.java
+++ mbtiTrip/src/main/java/com/example/test/POST/Service/PostService.java
@@ -1,31 +1,21 @@
 package com.example.test.POST.Service;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.domain.Specification;
 
-import com.example.test.POST.DTO.Post;
 import com.example.test.POST.DTO.PostDTO;
 import com.example.test.User.DTO.UserDTO;
 
-import groovyjarjarantlr4.v4.runtime.atn.SemanticContext.Predicate;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
-
 public interface PostService {
 
-//	public Predicate toPredicate(Root<Post> q, CriteriaQuery<?> query, CriteriaBuilder cb);
-	
-//	public Page<PostDTO> getList(int page, String kw);
-	
-//	public PostDTO create(String title, String content, UserDTO user);
-//	
-//	public PostDTO modify(PostDTO postDTO, String title, String content);
-//	
-//	public void delete(PostDTO postDTO);
-//	
-//	public PostDTO vote(PostDTO postDTO, UserDTO userDTO);
-//
-//	public PostDTO getPost(Integer id);
-//	
+	 public Page<PostDTO> getList(int page, String kw);
+	 
+	 public PostDTO getPost(Integer userid);
+	 
+	 public PostDTO create(String title, String content, UserDTO user);
+	 
+	 public PostDTO modify(PostDTO postDto, String title, String content);
+	 
+	 public void delete(PostDTO postDto);
+	 
+	 public PostDTO vote(PostDTO postDto, UserDTO userDto);
 }
