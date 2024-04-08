2d7161e0669552db42e2e86e9387261d5b4b33c7
user <user@DESKTOP-F14IM2U>
2024-04-08 12:41:22 +0900

    u-0408 최우진

--- mbtiTrip/src/main/resources/static/main/main.js
+++ mbtiTrip/src/main/resources/static/main/main.js
@@ -1,3 +1,17 @@
+
+var sakura = new Sakura('.mainHeader',{fallSpeed :0.7});
+
+
+
+setInterval(()=>{
+	var sakuraCtn = document.querySelectorAll('.sakura');
+	if(sakuraCtn.length>50){
+		sakura.stop(true);
+	}else if(sakuraCtn.length<10){
+		sakura.start();
+	}
+},10000);
+
 var container = document.getElementById('map1'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
@@ -5,5 +19,3 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
- var sakura = new Sakura('.mainHeader');
