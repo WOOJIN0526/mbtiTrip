edd8c019ef3122902c2b98a8f6d248e66b4b8632
hyunseok942 <9417743@naver.com>
2024-04-11 13:53:05 +0900

    u-0411 김현석

--- mbtiTrip/src/main/java/com/example/test/replace/Service/ReplaceCategoryService.java
+++ mbtiTrip/src/main/java/com/example/test/replace/Service/ReplaceCategoryService.java
@@ -1,18 +1,18 @@
-package com.example.test.replace.Service;
-
-import java.util.List;
-
-
-import com.example.test.replace.DTO.ReplaceCategoryDTO;
-
-public interface ReplaceCategoryService {
-
-
-	
-	
-	public List<ReplaceCategoryDTO> getList();
-
-	public ReplaceCategoryDTO getCategory(Integer replaceCategoryDTO);
-
-	
-}
+//package com.example.test.replace.Service;
+//
+//import java.util.List;
+//
+//
+//import com.example.test.replace.DTO.ReplaceCategoryDTO;
+//
+//public interface ReplaceCategoryService {
+//
+//
+//	
+//	
+//	public List<ReplaceCategoryDTO> getList();
+//
+//	public ReplaceCategoryDTO getCategory(Integer replaceCategoryDTO);
+//
+//	
+//}
